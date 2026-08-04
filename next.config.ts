0e1e0db4f1344b2460a8570d617dc81d1b60978c
anupsr <sranup007@gmail.com>
2026-08-04 10:24:02 +0530

    new changes

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,6 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   experimental: {
-//     turbopack: {
-//       root: "D:/Websites-Changes",
-//     },
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "images.unsplash.com",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: "D:/Websites-Changes/publicwebsite-mercuryacademy",
-  },
-
   images: {
     remotePatterns: [
       {
